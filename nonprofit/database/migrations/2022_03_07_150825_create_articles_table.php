4f0aea33f67ed56d1eb632f4023f3cb38a3b35f4
zdenek <z_pohludka@utb.cz>
2022-03-08 09:37:34 +0100

    DB tables + readme update

--- nonprofit/database/migrations/2022_03_07_150825_create_articles_table.php
+++ nonprofit/database/migrations/2022_03_07_150825_create_articles_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->integer('author_id');
+            $table->integer('category_id');
+            $table->string('title');
+            $table->text('text');
             $table->timestamps();
         });
     }
